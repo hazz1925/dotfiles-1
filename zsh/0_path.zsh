ab291cdac1aaad9c047f7ce3bebe8b3b454895e5
Hazeem Salim <hazeem.1925@gmail.com>
2024-05-11 18:42:04 +0800

    Add nvm paths

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -14,3 +14,8 @@ pathAppend "$HOME/.yadr/bin"
 pathAppend "$HOME/.yadr/bin/yadr"
 
 PATH="/opt/homebrew/bin:$PATH"
+
+# nvm path
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
